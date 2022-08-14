37d558d773dda35c84a6e51b592b0c3e7110aaca
MagicExist <92339104+MagicExist@users.noreply.github.com>
2022-08-14 18:24:11 -0500

    Update OS_II_Reto.py

--- OS_II_Reto.py
+++ OS_II_Reto.py
@@ -1,8 +1,11 @@
+#Importamos la libreria "pygame"
 import pygame
 
 
+#inicializamos la libreria
 pygame.init()
 
+#creamos una pantalla
 screen = pygame.display.set_mode((800,400))
 
 
@@ -11,15 +14,19 @@ screen = pygame.display.set_mode((800,400))
 while True:
 
     
-
+    #creamos el evento "quit" para poder cerrar la pantalla
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
 
+            
+    #condicional la cual si se presiona la tecla espacio imprime en pantalla un mensaje
     keybind = pygame.key.get_pressed()
     if keybind[pygame.K_SPACE]:
         print("Hola Mundo")
     else: print("Hola")
 
 
+        
+    #FPS
     pygame.display.update()
